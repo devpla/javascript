d815dfb0dd63c5e97dad74ae58d0c86c3b3fdc1d
devpla <chaeyeonhee@kakao.com>
2022-03-05 22:14:48 +0900

    feat: Keyboard mouse event 적용

--- keyboard/src/js/keyboard.js
+++ keyboard/src/js/keyboard.js
@@ -4,7 +4,9 @@ export class Keyboard {
   #containerEl;
   #keyboardEl;
   #inputGroupEl;
-  // #inputEl;
+  #inputEl;
+  #keyPress = false;
+  #mouseDown = false;
 
   constructor() {
     this.#assignElement();
@@ -17,7 +19,7 @@ export class Keyboard {
     this.#fontSelectEl = this.#containerEl.querySelector('#font');
     this.#keyboardEl = this.#containerEl.querySelector('#keyboard');
     this.#inputGroupEl = this.#containerEl.querySelector('#input-group');
-    // this.#inputEl = this.#inputGroupEl.querySelector('#input');
+    this.#inputEl = this.#inputGroupEl.querySelector('#input');
   }
 
   #addEvent() {
@@ -26,6 +28,35 @@ export class Keyboard {
     document.addEventListener('keydown', this.#onKeyDown.bind(this));
     document.addEventListener('keyup', this.#onKeyUp.bind(this));
     // this.#inputEl.addEventListener('input', this.#onInput);
+    this.#keyboardEl.addEventListener(
+      'mousedown',
+      this.#onMouseDown.bind(this)
+    );
+    document.addEventListener('mouseup', this.#onMouseUp.bind(this));
+  }
+
+  #onMouseUp(event) {
+    if (this.#keyPress) return;
+    this.#mouseDown = false;
+    const keyEl = event.target.closest('div.key');
+    const isActive = !!keyEl?.classList.contains('active');
+    const val = keyEl?.dataset.val;
+    if (isActive && !!val && val !== 'Space' && val !== 'Backspace') {
+      this.#inputEl.value += val;
+    }
+    if (isActive && val === 'Space') {
+      this.#inputEl.value += ' ';
+    }
+    if (isActive && val === 'Backspace') {
+      this.#inputEl.value = this.#inputEl.value.slice(0, -1);
+    }
+    this.#keyboardEl.querySelector('.active')?.classList.remove('active');
+  }
+
+  #onMouseDown(event) {
+    if (this.#keyPress) return;
+    this.#mouseDown = true;
+    event.target.closest('div.key')?.classList.add('active');
   }
 
   // #onInput(event) {
@@ -33,6 +64,8 @@ export class Keyboard {
   // }
 
   #onKeyDown(event) {
+    if (this.#mouseDown) return;
+    this.#keyPress = true;
     this.#inputGroupEl.classList.toggle(
       'error',
       /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)
@@ -43,6 +76,8 @@ export class Keyboard {
   }
 
   #onKeyUp(event) {
+    if (this.#mouseDown) return;
+    this.#keyPress = false;
     this.#keyboardEl
       .querySelector(`[data-code=${event.code}]`)
       ?.classList.remove('active');
